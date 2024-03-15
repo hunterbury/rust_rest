012d640f996b0f8f7dbdca74a063962990b2d48e
hunterbury <huntersbury1@gmail.com>
2024-03-15 14:44:00 -0400

    project setup, api outline

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(tweet::list)
+            .service(tweet::get)
+            .service(tweet::create)
+            .service(tweet::delete)
+            .service(like::list)
+            .service(like::plus_one)
+            .service(like::minus_one)
+    })
+    .bind("0.0.0.0.9090")?
+    .run()
+    .await
+}
