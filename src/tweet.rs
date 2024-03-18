85db86179e25db839a0614f55e3cd2aa509ef131
hunterbury <huntersbury1@gmail.com>
2024-03-18 10:13:04 -0400

    add /tweets endpoints

---
+++ src/tweet.rs
@@ -0,0 +1,52 @@
+pub type Tweets = Response<Tweet>;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Tweet {
+    pub id: String,
+    pub created_at: DateTime<Utc>,
+    pub message: String,
+    pub liked: Vec<Like>,
+}
+
+impl Tweet {
+    pub fn new(message: String) -> Self {
+        Self {
+            id: Uuid::new_v4().to_string(),
+            created_at: Utc::now(),
+            message,
+            likes: vec![],
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TweetRequest {
+    pub message: Option<String>,
+}
+
+impl TweetRequest {
+    pub fn to_tweet(&self) -> Option<Tweet> {
+        match &self.message {
+            Some(message) => Some(Tweet::new(message.to_string())),
+            None => None,
+        }
+    }
+}
+
+#[get("/tweets")]
+pub async fn list() -> HttpResponse {
+    // TODO find tweets
+
+    let tweets = Tweets { results: vec![] };
+
+    HttpResponse::Ok()
+        .content_type(APPLICATION_JSON)
+        .json(tweets)
+}
+
+#[post("/tweets")]
+pub async fn create(tweet_req: Json<TweetRequest>) -> HttpResponse {
+    HttpResponse::Created()
+        .content_type(APPLICATION_JSON)
+        .json(tweet_req.to_tweet())
+}
