bec7a26801e81866886414e58707074d68fe8539
hunterbury <huntersbury1@gmail.com>
2024-03-18 16:38:58 -0400

    update deps, setup db

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::env;
+use std::{env, io};
+
+use actix_web::{middleware, App, HttpServer};
+use diesel::{r2d2::ConnectionManager, PgConnection};
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
 
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+
     HttpServer::new(|| {
         App::new()
+            .data(pool.clone())
             .wrap(middleware::Logger::default())
             .service(tweet::list)
             .service(tweet::get)
