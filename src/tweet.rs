2a2a07bfae09bdfe6a71fab1671deeb18bd345a0
hunterbury <huntersbury1@gmail.com>
2024-03-19 09:46:18 -0400

    add tweet crud helper functions

--- src/tweet.rs
+++ src/tweet.rs
@@ -73,3 +73,55 @@ pub async fn delete(path: Path<(String,)>) -> HttpResponse {
         .await
         .unwrap()
 }
+
+fn list_tweets(total_tweets: i64, conn: &DBPooledConnection) -> Result<Tweets, Error> {
+    use create::schema::tweets::dsl::*;
+
+    let _tweets = match tweets
+        .order(created_at.desc())
+        .limit(total_tweets)
+        .load::<TweetDB>(conn)
+    {
+        Ok(tws) => tws,
+        Err(_) => vec![]
+    };
+
+    Ok(Tweets {
+        results: _tweets
+            .into_iter()
+            .map(|t| t.to_tweet())
+            .collect::<Vec<Tweet>>(),
+    })
+}
+
+fn find_tweet(_id: Uuid, conn: &DBPooledConnection) -> Result<Tweet, Error> {
+    use create::schema::tweets::dsl::*;
+
+    let res = tweets.filter(id.eq(_id)).load::<TweetDB>(conn);
+    match res {
+        Ok(tweets_db) => match tweets_db.first() {
+            Some(tweet_db) => Ok(tweet_db.to_tweet()),
+            _ => Err(Error::NotFound),
+        },
+        Err(err) => Err(err),
+    }
+}
+
+fn create_tweet(tweet: Tweet, conn: &DBPooledConnection) -> Result<Tweet, Error> {
+    use create::schema::tweets::dsl::*;
+
+    let tweet_db = tweet::to_tweet_db();
+    let _ = diesel::insert_into(tweets).values(&tweet_db).execute(conn);
+
+    Ok(tweet_db.to_tweet())
+}
+
+fn delete_tweet(_id: Uuid, conn: &DBPooledConnection) -> Result<(), Error> {
+    use create::schema::tweets::dsl::*;
+
+    let res = diesel::delete(tweets.filter(id.eq(_id))).execute(conn);
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err),
+    }
+}
