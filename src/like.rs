b557b0352b0ea94cdf309b5989c6e63ba77980fe
hunterbury <huntersbury1@gmail.com>
2024-03-19 10:36:47 -0400

    add crud functions for like

--- src/like.rs
+++ src/like.rs
@@ -40,3 +40,57 @@ pub async fn minus_one(path: Path<(String,)>) -> HttpResponse {
         .await
         .unwrap()
 }
+
+pub fn list_likes(_tweet_id: Uuid, conn: &DBPooledConnection) -> Result<Likes, Error> {
+    use create::schema::likes::dsl::*;
+
+    let _likes: Vec<LikeDB> = match likes
+        .filter(tweet_id.eq(_tweet_id))
+        .order(created_at.desc())
+        .load::<LikeDB>(conn)
+    {
+        Ok(lks) => lks,
+        Err(_) => vec![],
+    };
+
+    Ok(Likes {
+        results: _likes
+            .into_iter()
+            .map(|l| l.to_like())
+            .collect::<Vec<Like>>(),
+    })
+}
+
+pub fn create_like(_tweet_id: Uuid, conn: &DBPooledConnection) -> Result<Like, Error> {
+    use create::schema::likes::dsl::*;
+
+    let like = Like::new();
+    let _ = diesel::insert_into(likes)
+        .values(like.to_like_db(_tweet_id))
+        .execute(conn);
+
+    Ok(like)
+}
+
+pub fn delete_like(_tweet_id: Uuid, conn: &DBPooledConnection) -> Result<(), Error> {
+    use create::schema::likes::dsl::*;
+
+    let _likes = list_likes(_tweet_id, conn);
+
+    let like = match &_likes {
+        Ok(_likes) if !_likes.results.is_empty() => _likes.results.first(),
+        _ => None,
+    };
+
+    if like.is_none() {
+        return Ok(());
+    }
+
+    let like_id = Uuid::from_str(like.unwrap().id.as_str()).unwrap();
+
+    let res = diesel::delete(likes.filter(id.eq(like_id))).execute(conn);
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err),
+    }
+}
